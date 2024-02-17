71698daafbe13194228122c4c05c048f5486f3b7
amir <mashhadifarahani1@gmail.com>
2024-02-17 18:44:33 +0330

    adding new project card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
     const d = new Date().getFullYear() 
 
@@ -9,7 +10,9 @@ const Footer = () => {
         <div className='nav-bg flex lg:flex-row flex-col justify-between gap-y-4 items-center py-8 px-20   shadow-2xl'>
             <h1 className="text-white">Copyright © {d}. All rights are reserved</h1>
             <div className="flex flex-row gap-x-8 ">
-            <FaGithub color="white" size={25}/>
+            <a href="https://github.com/AFra1892">
+                <FaGithub  color="white" size={25}/>
+            </a>
             <FaLinkedin color="white" size={25} />
             </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import pizza from '../assets/burger-land.png'
 import gpt3 from '../assets/gpt3-full.png'
+import socialMedia from '../assets/social-media.png'
 const Projects = () => {
   return (
     <>
     <div id='projects' className='flex flex-col justify-center items-center gap-y-8 my-32'>
     <h1 className='lg:text-[55px] text-2xl  pl-8'>Projects</h1>
+
+
         <div className='nav-bg bs flex lg:flex-row flex-col  rounded-xl p-8 lg:gap-x-8 gap-y-8 lg:w-[900px] w-[400px]'>
                 <div className="lg:w-2/3 w-full h-[300px] overflow-scroll">
                     <img className=' rounded-xl' src={pizza} alt='pizz'/>
@@ -35,6 +38,7 @@ const Projects = () => {
 
                 </div>
             </div>
+            {/* FIRST CARD */}
             <div className='nav-bg flex lg:flex-row flex-col bs rounded-xl p-8 gap-x-8 lg:w-[900px] w-[400px]'>
                 <div className="lg:w-2/3 w-full h-[300px] overflow-scroll">
                     <img className=' rounded-xl' src={gpt3} alt='pizz'/>
@@ -57,6 +61,37 @@ const Projects = () => {
                     </a>
                 </div>
             </div>
+            {/* SECOND CARD */}
+            <div className='nav-bg bs flex lg:flex-row flex-col  rounded-xl p-8 lg:gap-x-8 gap-y-8 lg:w-[900px] w-[400px]'>
+                <div className="lg:w-2/3 w-full h-[300px] overflow-scroll">
+                    <img className=' rounded-xl' src={socialMedia} alt='pizz'/>
+                </div>
+                <div className="flex flex-col gap-y-4">
+                    <h3 className='font-bold text-xl'>Social Media Clone (February 2024)</h3>
+                    <p className='w-[300px]'>a responsive single page app with login , posting pictures and text , likes and ... feature.storing users data and posts in database. </p>
+                    <div className="grid grid-cols-3 gap-4">
+                        <div className='flex justify-center black-bg p-2 rounded-lg shadow-md w-20 content-center'>
+                            <p className='text-lg'>React</p>
+                        </div>
+                        <div className='flex justify-center black-bg p-2 rounded-lg shadow-md w-20 content-center'>
+                            <p className='text-lg'>Node</p>
+                        </div>
+                        <div className='flex justify-center black-bg p-2 rounded-lg shadow-md w-20 content-center'>
+                            <p className='text-lg'>Express</p>
+                        </div>
+                        <div className='flex justify-center black-bg p-2 rounded-lg shadow-md w-20 content-center'>
+                            <p className='text-md'>MongoDB</p>
+                        </div>
+                    </div>
+                        <a href='https://github.com/AFra1892/social-media-mern.git'>
+                        <div className='special-bg main-color text-xl flex justify-center p-2 rounded-lg  content-center'>
+                            Github
+                        </div>  
+                        </a>
+
+                </div>
+            </div>
+            {/* THIRD CARD */}
     </div>
     </>
   )
